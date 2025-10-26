7004d6b613eefc62d917867d1bb73e41c79dd27b
Shahid Ali <bajeershahid125@gmail.com>
2025-10-26 09:01:01 -0700

    success.cpp-Practice

--- success.cpp
+++ success.cpp
@@ -2,9 +2,23 @@
 using namespace std;
 
 int main() {
-    int arr[5] = {10, 20, 30, 40, 50};
-    for(int i = 0; i<5; i++) {
-        cout <<"Element" << i << ":" << arr[i] <<endl;
+    int arr[5];
+    cout << "Enter 5 numbers: " << endl;
+for (int i = 0; i < 5; i++) {
+    cin >> arr[i];
     }
-    return 0;
-}
+
+    // Step: 2 Assume first element is largest
+    int maxVal = arr[0];
+
+    //Step 3 Campare each element with maxVal
+    for (int i = 1; i < 5; i++) {
+        if (arr[i] > maxVal) {
+            maxVal = arr[i]; // Update maxVal if Largest number found
+        }
+    }
+   // Step : 4 Display result
+   cout << "The Largest numberis:" << maxVal << endl;
+
+   return 0;
+} 
